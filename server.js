7caa46d4ef2cd23a556ba7e9835012af42531fd2
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-08 17:24:45 -0400

    setting up server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
+
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+  );
+  
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  );
+
+
+  
