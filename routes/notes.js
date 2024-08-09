a8b77598d21ee2861a88ec44d40df0665b3b75b1
otekolste <58699635+otekolste@users.noreply.github.com>
2024-08-09 17:25:43 -0400

    implemented post method for adding notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,58 @@
 const notes = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const util = require('util');
 
 notes.get('/', (req,res) => {
-    fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    fs.readFile('./db/db.json','utf8', (err, data) => {
+        if(err) {console.error(err)}
+        else {
+            try {
+                res.json(JSON.parse(data));
+            }
+            catch(e) {
+                res.json('');
+            }
+        }
+
+    })
 })
+
+notes.post('/', (req,res) => {
+    console.log('hi');
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            noteID: uuidv4(),
+        };
+    console.log(newNote);
+    console.log(JSON.stringify(newNote));
+    fs.readFile('./db/db.json', 'utf8', (err,data) => {
+        if(err) {console.error(err)}
+        else {
+            let notesArray;
+            try{            
+             notesArray = JSON.parse(data);
+            }
+            catch(e) {
+                notesArray = [];
+            }
+            notesArray.push(newNote);
+            fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err) => err ? console.error(err) : res.json(newNote));
+
+        }
+
+    });
+
+    }
+    else {
+        res.error('An error occurred in attempting to add the tip.');
+    }
+
+})
+
 
 module.exports = notes;
